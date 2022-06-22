9c2bb5d97adcf7085edfd91f3bdbde515eda9890
Ewa Namysł <e.k.namysl@gmail.com>
2022-06-22 17:51:05 +0200

    o -> out

--- linux_scripts/compile.sh
+++ linux_scripts/compile.sh
@@ -2,6 +2,6 @@
 
 #usage: ./compile gui
 
-gcc -o $1.o $1.c `pkg-config --cflags --libs gtk+-3.0 libnotify`
+gcc -o $1.out $1.c `pkg-config --cflags --libs gtk+-3.0 libnotify`
 
-chmod +x $1.o
+chmod +x $1.out
