4ef1541bdd96cf6491c3864993bcca1b16448595
Ewa Namysł <e.k.namysl@gmail.com>
2022-06-22 15:26:59 +0200

    -o flag fix

--- linux_scripts/compile.sh
+++ linux_scripts/compile.sh
@@ -2,6 +2,6 @@
 
 #usage: ./compile gui
 
-gcc -o $1 $1.c `pkg-config --cflags --libs gtk+-3.0 libnotify`
+gcc -o $1.o $1.c `pkg-config --cflags --libs gtk+-3.0 libnotify`
 
-chmod +x $1
+chmod +x $1.o
